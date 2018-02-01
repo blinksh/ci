af99a3e32cdd6e63f98064dbf064b8e0a8b0a0b3
carloscabanero <carloscabanero@users.noreply.github.com>
2018-02-01 13:56:03 -0500

    Update build-blink.sh

--- build-blink.sh
+++ build-blink.sh
@@ -2,7 +2,7 @@
 
 # Compile as a worker
 chown -RHL worker ..
-su worker -c "./blink/get_frameworks.sh && fastlane gym -c \
+su worker -c "./blink/get_frameworks.sh && /usr/local/bin/fastlane gym -c \
     --scheme Blink \
     --project ./Blink/Blink.xcodeproj \
     --output_directory ./release/"
