d822c4c9cd9f8515b02d1c239f45add31bc3613c
carloscabanero <carloscabanero@users.noreply.github.com>
2018-03-04 16:20:27 -0500

    Tar.gz directory flag

--- build-libmoshios.sh
+++ build-libmoshios.sh
@@ -5,6 +5,6 @@
 VERSION=$(grep PACKAGE_VERSION build-mosh/mosh/config.h | sed "s/^.*PACKAGE_VERSION.*\"\(.*\)\"/\1/")
 BUILD=$(grep BUILD_VERSION build-mosh/mosh/version.h | sed "s/^.*VERSION.*\"\(.*\)\"/\1/")
 
-cd build-mosh/ && tar -zcvf ../release/libmoshios-$VERSION-$BUILD.framework.tar.gz libmoshios.framework && cd ..
+tar -zcvf release/libmoshios-$VERSION-$BUILD.framework.tar.gz --directory=build-mosh/ libmoshios.framework
 
 echo $VERSION-$BUILD > release/VERSION
