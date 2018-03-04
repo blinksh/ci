45480242b31f9c36be2bbb71a6c2a1138b9b1397
Carlos Cabanero <cc@blink.sh>
2018-03-04 16:53:06 -0500

    Updated build-blink for new concourse

--- build-blink.sh
+++ build-blink.sh
@@ -3,11 +3,9 @@
 PATH=$PATH:/usr/local/bin
 ./unlock_keychain.sh > /dev/null
 
-# Compile as a worker
-chown -RHL worker ..
-su worker -c "./blink/get_frameworks.sh && fastlane gym -c \
+./blink/get_frameworks.sh && fastlane gym -c \
     --scheme Blink \
     --project ./Blink/Blink.xcodeproj \
-    --output_directory ./release/"
+    --output_directory ./release/
 #    --build_path ./release/
 # profiles, signing, team, etc...
