0b97bd1a66811339542478fa14f4fd6bdca288b6
Carlos Cabanero <cc@blink.sh>
2018-03-04 19:08:10 -0500

    Cleaning up and adding special fonts

--- build-blink.sh
+++ build-blink.sh
@@ -3,6 +3,8 @@
 PATH=$PATH:/usr/local/bin
 ./ci/unlock_keychain.sh > /dev/null
 
+cp -v ./pragmatapro/* ./blink/Resources/Fonts/
+
 ./blink/get_frameworks.sh && fastlane gym -c \
     --scheme Blink \
     --project ./Blink/Blink.xcodeproj \
